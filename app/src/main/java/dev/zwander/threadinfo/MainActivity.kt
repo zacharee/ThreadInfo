da33337106aa9149f5ba83695c4b41377ace2ada
Zachary Wander <zachary.wander@gmail.com>
2026-07-06 18:24:49 -0400

    Rework fetching, add pull to refresh, add find by PAN ID

--- app/src/main/java/dev/zwander/threadinfo/MainActivity.kt
+++ app/src/main/java/dev/zwander/threadinfo/MainActivity.kt
@@ -10,26 +10,38 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.tasks.Task
 import com.google.android.gms.threadnetwork.GetAllActiveCredentialsRequest
+import com.google.android.gms.threadnetwork.IntentSenderResult
 import com.google.android.gms.threadnetwork.ThreadNetwork
 import com.google.android.gms.threadnetwork.ThreadNetworkCredentials
 import dev.zwander.threadinfo.ui.theme.ThreadInfoTheme
@@ -43,6 +55,8 @@ class MainActivity : ComponentActivity() {
         } else {
             Log.e("TI", "Canceled")
         }
+
+        stopLoadingIfPossible()
     }
     val preferredLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
         if (it.resultCode == RESULT_OK) {
@@ -51,7 +65,16 @@ class MainActivity : ComponentActivity() {
             Log.e("TI", "Canceled")
         }
 
-        loading = false
+        stopLoadingIfPossible()
+    }
+    val byIdLauncher = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            networks += ThreadNetworkCredentials.fromIntentSenderResultData(it.data!!)
+        } else {
+            Log.e("TI", "Canceled")
+        }
+
+        showingPanIdDialog = false
     }
     val saveLauncher = registerForActivityResult(ActivityResultContracts.CreateDocument("*/*")) { uri ->
         uri?.let {
@@ -63,31 +86,56 @@ class MainActivity : ComponentActivity() {
 
     var networks by mutableStateOf(listOf<ThreadNetworkCredentials>())
     var loading by mutableStateOf(true)
+    var showingPanIdDialog by mutableStateOf(false)
     var pendingData: ByteArray? = null
+
+    val tasks = mutableListOf<Task<IntentSenderResult>>()
+
+    private fun stopLoadingIfPossible() {
+        if (tasks.all { it.isComplete || it.isCanceled }) {
+            loading = false
+            tasks.clear()
+        }
+    }
+
+    private fun runTasks() {
+        loading = true
+        networks = listOf()
+
+        tasks.add(
+            ThreadNetwork.getNetworkClient(this)
+                .preferredCredentials
+                .addOnSuccessListener { preferredResult ->
+                    preferredResult.intentSender?.let {
+                        preferredLauncher.launch(IntentSenderRequest.Builder(it).build())
+                    } ?: stopLoadingIfPossible()
+                }
+                .addOnFailureListener {
+                    Log.e("TI", "Failed preferred", it)
+                    stopLoadingIfPossible()
+                },
+        )
+
+        tasks.add(
+            ThreadNetwork.getNetworkClient(this)
+                .getAllActiveCredentials(GetAllActiveCredentialsRequest.newBuilder().build())
+                .addOnSuccessListener { activeResult ->
+                    activeResult.intentSender?.let {
+                        activeLauncher.launch(IntentSenderRequest.Builder(it).build())
+                    } ?: stopLoadingIfPossible()
+                }
+                .addOnFailureListener {
+                    Log.e("TI", "Failed active", it)
+                    stopLoadingIfPossible()
+                },
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        ThreadNetwork.getNetworkClient(this)
-            .getAllActiveCredentials(GetAllActiveCredentialsRequest.newBuilder().build())
-            .addOnSuccessListener { activeResult ->
-                activeLauncher.launch(IntentSenderRequest.Builder(activeResult.intentSender!!).build())
-
-                ThreadNetwork.getNetworkClient(this)
-                    .preferredCredentials
-                    .addOnSuccessListener { preferredResult ->
-                        preferredLauncher.launch(IntentSenderRequest.Builder(preferredResult.intentSender!!).build())
-                    }
-                    .addOnFailureListener {
-                        Log.e("TI", "Failed", it)
-                        loading = false
-                    }
-            }
-            .addOnFailureListener {
-                Log.e("TI", "Failed", it)
-                loading = false
-            }
+        runTasks()
 
         setContent {
             ThreadInfoTheme {
@@ -98,27 +146,64 @@ class MainActivity : ComponentActivity() {
                         targetState = !loading,
                     ) { hasNetworks ->
                         if (hasNetworks) {
-                            LazyColumn(
-                                modifier = Modifier.fillMaxSize(),
-                                contentPadding = WindowInsets.systemBars.asPaddingValues(),
+                            PullToRefreshBox(
+                                isRefreshing = false,
+                                onRefresh = { runTasks() },
                             ) {
-                                items(items = networks.distinctBy { it.panId }, key = { it.networkName }) { network ->
-                                    Row(
+                                Column(
+                                    modifier = Modifier.fillMaxSize(),
+                                ) {
+                                    LazyColumn(
                                         modifier = Modifier.fillMaxWidth()
-                                            .heightIn(min = 64.dp)
-                                            .clickable {
-                                                pendingData = network.activeOperationalDataset
-                                                saveLauncher.launch("${network.networkName}.tlv")
-                                            }
-                                            .padding(
-                                                horizontal = 8.dp,
-                                                vertical = 16.dp,
-                                            ),
-                                        verticalAlignment = Alignment.CenterVertically,
+                                            .weight(1f),
+                                        contentPadding = WindowInsets.systemBars
+                                            .only(
+                                                WindowInsetsSides.Top +
+                                                        WindowInsetsSides.Start +
+                                                        WindowInsetsSides.End,
+                                            )
+                                            .asPaddingValues(),
                                     ) {
-                                        Text(
-                                            text = network.networkName,
-                                        )
+                                        items(items = networks.distinctBy { it.panId }, key = { it.networkName }) { network ->
+                                            Row(
+                                                modifier = Modifier.fillMaxWidth()
+                                                    .heightIn(min = 64.dp)
+                                                    .clickable {
+                                                        pendingData = network.activeOperationalDataset
+                                                        saveLauncher.launch("${network.networkName}.tlv")
+                                                    }
+                                                    .padding(
+                                                        horizontal = 8.dp,
+                                                        vertical = 16.dp,
+                                                    ),
+                                                verticalAlignment = Alignment.CenterVertically,
+                                            ) {
+                                                Text(
+                                                    text = network.networkName,
+                                                )
+                                            }
+                                        }
+                                    }
+
+                                    Box(
+                                        modifier = Modifier.fillMaxWidth()
+                                            .padding(16.dp)
+                                            .padding(
+                                                WindowInsets.systemBars
+                                                    .only(
+                                                        WindowInsetsSides.Bottom,
+                                                    )
+                                                    .asPaddingValues()
+                                            ),
+                                        contentAlignment = Alignment.Center,
+                                    ) {
+                                        OutlinedButton(
+                                            onClick = {
+                                                showingPanIdDialog = true
+                                            },
+                                        ) {
+                                            Text(text = stringResource(R.string.add_by_pan_id))
+                                        }
                                     }
                                 }
                             }
@@ -130,6 +215,68 @@ class MainActivity : ComponentActivity() {
                                 CircularProgressIndicator()
                             }
                         }
+                    }
+
+                    if (showingPanIdDialog) {
+                        var fieldText by remember {
+                            mutableStateOf("")
+                        }
+
+                        AlertDialog(
+                            onDismissRequest = {
+                                showingPanIdDialog = false
+                            },
+                            title = { Text(text = stringResource(R.string.add_by_pan_id)) },
+                            text = {
+                                Column {
+                                    OutlinedTextField(
+                                        value = fieldText,
+                                        onValueChange = {
+                                            fieldText = it
+                                        },
+                                        placeholder = {
+                                            Text(text = stringResource(R.string.pan_id_hint))
+                                        },
+                                    )
+
+                                    Text(
+                                        text = stringResource(R.string.add_by_pan_id_desc),
+                                    )
+                                }
+                            },
+                            confirmButton = {
+                                TextButton(
+                                    onClick = {
+                                        ThreadNetwork.getNetworkClient(this)
+                                            .getCredentialsByExtendedPanId(fieldText.split(",").map { it.toByte() }.toByteArray())
+                                            .addOnSuccessListener { result ->
+                                                result.intentSender?.let {
+                                                    byIdLauncher.launch(
+                                                        IntentSenderRequest.Builder(it).build(),
+                                                    )
+                                                } ?: run {
+                                                    showingPanIdDialog = false
+                                                }
+                                            }
+                                            .addOnFailureListener {
+                                                Log.e("TI", "Failed to add by PAN ID", it)
+                                                showingPanIdDialog = false
+                                            }
+                                    },
+                                ) {
+                                    Text(text = stringResource(R.string.add))
+                                }
+                            },
+                            dismissButton = {
+                                TextButton(
+                                    onClick = {
+                                        showingPanIdDialog = false
+                                    },
+                                ) {
+                                    Text(text = stringResource(R.string.cancel))
+                                }
+                            },
+                        )
                     }
                 }
             }
